546150edfa1e97651dfac6776c1dde1f95f78c1d
CHOIHEEJAE <gmlwochl123@gmail.com>
2022-12-02 17:23:20 +0900

    사각형그려보기

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d'); // 그림을 그릴 수 있는 context 변수화
+
+canvas.width = 800;
+canvas.height = 800;
+
+
+ctx.fillRect(50, 50, 100, 100);
+ctx.strokeRect(150, 50, 100, 100);
